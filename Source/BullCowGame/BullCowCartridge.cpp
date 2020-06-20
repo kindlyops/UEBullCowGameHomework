58ecb0e9f2eea820f32de43e0fbd9bd029677d6a
Elliot Murphy <statik@users.noreply.github.com>
2020-06-19 23:45:44 -0400

    Cooooode
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,11 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("Welcome to the game!");
+    PrintLine("Please enter a word and press enter:\n");
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
-
+    ClearScreen();
 }
